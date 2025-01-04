ff2a9c0ea40fb66aef91829a939f2f84485a5900
Jamiu Olaroungbe <olaabiodun157@gmail.com>
2025-01-04 11:58:07 +0100

    Initial commit

--- App.tsx
+++ App.tsx
@@ -2,8 +2,12 @@ import './global.css';
 
 import 'react-native-gesture-handler';
 
-import RootStack from './navigation';
+import { View, Text } from 'react-native';
 
 export default function App() {
-  return <RootStack />;
+  return (
+    <View className="flex-1 items-center justify-center">
+      <Text>Hello World</Text>
+    </View>
+  )
 }
